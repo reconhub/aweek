7c394b6fbc994f912601475c0508b5040da80c23
Zhian N. Kamvar <zkamvar@gmail.com>
2019-06-10 13:00:30 +0100

    add tests for factor fencepost
    this will address #22

--- tests/testthat/test-factor_aweek.R
+++ tests/testthat/test-factor_aweek.R
@@ -1,6 +1,4 @@
 context("factorisation tests")
-dat <- structure(c(17965, 17960, 17954, 17956, 17955, 17952, 17958, 
-                   17952, 17958, 17957), class = "Date")
 
 test_that("factor_aweek will reject non-aweek objects", {
 
@@ -10,6 +8,24 @@ test_that("factor_aweek will reject non-aweek objects", {
 
 test_that("factor_aweek accounts for edge weeks", {
 
-  expect_identical(levels(factor_aweek(as.aweek(dat))), c("2019-W09", "2019-W10", "2019-W11"))
+  w1 <- get_aweek(c(8, 11), year = 2019, day = c(7, 1))
+  w2 <- get_aweek(c(8, 11), year = 2019, day = c(1, 7))
+
+  f1 <- factor_aweek(w1)
+  f2 <- factor_aweek(w2)
+  expect_identical(levels(f1), c("2019-W08", "2019-W09", "2019-W10", "2019-W11"))
+  expect_identical(levels(f2), c("2019-W08", "2019-W09", "2019-W10", "2019-W11"))
+
+})
+
+test_that("factor_aweek accounts for edge days across years", {
+
+  w3 <- get_aweek(c(53, 02), year = 2015:2016, day = c(7, 1))
+  w4 <- get_aweek(c(53, 02), year = 2015:2016, day = c(1, 7))
+  
+  f3 <- factor_aweek(w3)
+  f4 <- factor_aweek(w4)
+  expect_identical(levels(f3), c("2015-W53", "2016-W01", "2016-W02"))
+  expect_identical(levels(f4), c("2015-W53", "2016-W01", "2016-W02"))
 
 })
